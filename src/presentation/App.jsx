5debefc382fe6afceb788b8cc141eb9230950eca
woachkatzl <y.v.polonskaya@gmail.com>
2023-12-10 23:35:35 -0500

    simple router example

--- src/presentation/App.jsx
+++ src/presentation/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { Button } from "./ui-kit/button";
 import { Checkbox1 } from "./ui-kit/checkbox";
@@ -10,12 +11,52 @@ function onButtonClick() {
 
 function App() {
   return (
-    <>
-      <Button onClick={onButtonClick}>Кнопка React</Button>
-      <Checkbox1 />
-      <Timer />
-    </>
+    <Router>
+      <div>
+        <Button onClick={onButtonClick}>Кнопка React</Button>
+        <Checkbox1 />
+        <Timer />
+        <div style={{ border: "1px solid red" }}>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function NoMach() {
+  return <h2>404 Page not found</h2>;
 }
 
 export { App };
