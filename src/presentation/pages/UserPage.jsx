8083e5dd4121a47e77327406060f794ee0083a9a
woachkatzl <y.v.polonskaya@gmail.com>
2024-04-04 13:37:14 -0400

    Your commit message

--- src/presentation/pages/UserPage.jsx
+++ src/presentation/pages/UserPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import userImage from "../../assets/cat-avatar-2.png";
+
 function UserPage(props) {
   const { users } = props;
 
@@ -11,6 +13,7 @@ function UserPage(props) {
   return (
     <div>
       <h2>{user.name}</h2>
+      <img src={userImage} alt="User avatar" />
     </div>
   );
 }
