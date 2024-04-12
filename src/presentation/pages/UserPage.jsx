271fb28b4b0b0741c6778fcebf5b63ba2f52b68b
woachkatzl <y.v.polonskaya@gmail.com>
2024-04-12 11:00:07 -0400

    added a base modal for avatar change

--- src/presentation/pages/UserPage.jsx
+++ src/presentation/pages/UserPage.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { Modal } from "antd";
+
 import userImage from "../../assets/cat-avatar-2.png";
+import arrowLeft from "../../assets/arrow_left.svg";
+import arrowRight from "../../assets/arrow_right.svg";
 
 function UserPage(props) {
   const { users } = props;
 
   const { id } = useParams();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const user = users.find((user) => user.id == id);
 
   return (
     <div>
       <h2>{user.name}</h2>
-      <img src={userImage} alt="User avatar" />
+      <img
+        src={userImage}
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+        alt="User avatar"
+      />
+      {isModalOpen && (
+        <Modal
+          open={isModalOpen}
+          onCancel={() => setIsModalOpen(false)}
+          footer={null}
+          className="avatar-selection-modal"
+          width="620px"
+        >
+          <p className="avatarModuleText">Choose an avatar</p>
+          <div className="avatarModuleContent">
+            <button className="avatarModuleBtn">
+              <img src={arrowLeft} alt="Arrow" />
+            </button>
+            <img className="avatarModuleImg" src={userImage} alt="avatar" />
+            <button className="avatarModuleBtn">
+              <img src={arrowRight} alt="Arrow" />
+            </button>
+          </div>
+          <button>Save</button>
+        </Modal>
+      )}
     </div>
   );
 }
